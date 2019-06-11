f2b669d481241450275e2d069fa3f0f9b8dd20c0
mathiasVoelckerPUCRS <mathias.voelcker@acad.pucrs.br>
2019-06-10 21:43:28 -0300

    desquebrando a aplicação

--- App.java
+++ App.java
@@ -41,9 +41,8 @@ public class App {
 
     private static void start() throws InterruptedException, IOException {
         if (node.id == nodes.get(nodes.size() - 1).id) {
-            Coordenator coordenator = node;
             System.out.println("Node is manager");
-            coordenator.manageNodes();
+            manageMessages().start();
             
         } else {
             System.out.println("Node is not manager");
@@ -52,6 +51,28 @@ public class App {
             receiveMessage().start();
         }
         // Thread.sleep(10000);
+    }
+
+    public static Thread manageMessages() {
+        return new Thread(() -> {
+            while (true) {
+                byte[] input = new byte[256];
+                DatagramPacket packet = new DatagramPacket(input, input.length);
+                try {
+                    System.out.println("RECEIVING: " + node.host + "-" + node.port);
+                    socket.receive(packet);
+                    String received = new String(packet.getData(), 0, packet.getLength());
+                    System.out.println(received);
+                    String[] senderData = received.split("-");
+                    byte[] output = "okFromManager".getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(output, output.length,
+                            InetAddress.getByName(senderData[0]), Integer.parseInt(senderData[1]));
+                    socket.send(sendPacket);
+                } catch (NumberFormatException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private static Thread sendMessageToManager() throws IOException {
